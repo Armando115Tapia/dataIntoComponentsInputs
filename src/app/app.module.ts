4817af868f60e9bacb444d445c4b814aec3836e4
Armando <armandotapia115@hotmail.com>
2019-05-09 23:15:33 -0500

    Add some styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { PadreComponent } from './pages/padre/padre.component';
+import { FooterComponent } from './pages/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PadreComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
