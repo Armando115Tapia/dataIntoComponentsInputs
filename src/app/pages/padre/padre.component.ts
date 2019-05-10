36cb658b2401e509f4895928a2c78f50bcecfa2b
Armando <armandotapia115@hotmail.com>
2019-05-09 23:19:32 -0500

    Add some styles

--- src/app/pages/padre/padre.component.ts
+++ src/app/pages/padre/padre.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./padre.component.css']
 })
 export class PadreComponent implements OnInit {
+  public estudiante ;
 
   constructor() { }
 
